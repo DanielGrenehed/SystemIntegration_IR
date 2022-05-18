d298353cf267a8f54ea7e02f2f6cb76d7ee7704c
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2022-05-18 20:22:48 +0200

    Sensor OLED and signal validation

--- sensor/esp32_ir/ESP32_IR/ESP32_IR.ino
+++ sensor/esp32_ir/ESP32_IR/ESP32_IR.ino
@@ -1,11 +1,20 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <Wire.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <IRremote.h>
 #include <IRremoteInt.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #define USE_SERIAL Serial
+#define I2C_SDA 21
+#define I2C_SCL 22
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); 
 
 const int ir_receive_pin = 25;
 int sensor_read_success = 0;
@@ -36,14 +45,46 @@ void setOutCode(unsigned long value) {
   object["code"] = String(value, HEX);
 }
 
+const char* screen_header = "IR code:";
+
+void setScreenText(const char* text) {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setCursor(0,0);
+    display.println(text);
+    display.display();
+}
+
+unsigned long scan_code = 0;
+void updateScanCode(unsigned long code) {
+  if (code != scan_code) {
+    scan_code = code;
+    setScreenText(screen_header);
+    display.setCursor(0, 10);
+    display.setTextSize(2);
+    display.println(String(scan_code, HEX));
+    display.display();
+  }
+}
+void onInvalidIRCode(unsigned long code) {
+  USE_SERIAL.println("BAD CODE");
+}
+
+const unsigned long bad_code_mask = 0x000000FF;
 void scanIR() {
   if (IrReceiver.decode()) {
     unsigned long ir_value = IrReceiver.decodedIRData.decodedRawData;
     if (ir_value != 0) {
-      setOutCode(ir_value);
-      last_code = ir_value;
-      sensor_read_success = 1;
+      // fix invalid codes 
+      if (ir_value & bad_code_mask) onInvalidIRCode(ir_value);
+      else {
+        setOutCode(ir_value);
+        last_code = ir_value;
+        sensor_read_success = 1;
+        updateScanCode(ir_value);
       //USE_SERIAL.println(ir_value, HEX);
+      }
+      
     }
     IrReceiver.resume();
   }
@@ -74,9 +115,17 @@ void sendData() {
 }
 
 void setup() {
-
+    pinMode(oled_power_pin, OUTPUT);
+    digitalWrite(oled_power_pin, HIGH);
     USE_SERIAL.begin(115200);
-
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+      Serial.println(F("SSD1306 allocation failed"));
+    } else {
+      display.setTextColor(WHITE);
+      char* str = "ESP32_IR_OLED";
+      setScreenText(str);
+    }
+    
     USE_SERIAL.println();
     USE_SERIAL.println();
     USE_SERIAL.println();

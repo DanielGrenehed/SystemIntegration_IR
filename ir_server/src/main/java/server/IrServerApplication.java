30a9242d30b21ac7cf1bd7337c3d00a6c4577ba8
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2022-05-20 09:43:52 +0200

    Created server and database folders

---
+++ ir_server/src/main/java/server/IrServerApplication.java
@@ -0,0 +1,13 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrServerApplication.class, args);
+	}
+
+}

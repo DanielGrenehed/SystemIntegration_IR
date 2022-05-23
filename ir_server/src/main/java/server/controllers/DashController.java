1ee87cbceac9b56be23ea60df5a540cf225405c0
Wadzabi <martin.piqueras1@gmail.com>
2022-05-23 09:46:14 +0200

    Sensor dashboard

--- ir_server/src/main/java/server/controllers/DashController.java
+++ ir_server/src/main/java/server/controllers/DashController.java
@@ -4,15 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import server.sensor.SensorDAO;
 
 import java.sql.SQLException;
 
 @Controller
 public class DashController {
 
+	private SensorDAO dao = new SensorDAO();
+
 	@RequestMapping("/dash/{id}")
 	public String showDash(@PathVariable int id, Model model) throws SQLException {
 		model.addAttribute("sensor_id", id);
 		return "dash";
+	}
+
+	@RequestMapping("/")
+	public String showSensorDash(Model model) throws SQLException {
+		model.addAttribute("sensor_list", dao.getSensors());
+		return "sensordash";
 	}
 }
